507e3ac485dde239f0ac0bea4c50c3555c83d31f
Zoe Ingram <zoe.a.ingram@gmail.com>
2018-03-01 01:53:50 -0800

    adds ids to layers

--- public/scripts/partials/layers.js
+++ public/scripts/partials/layers.js
@@ -1,1 +1,10 @@
-console.log("layers partial js");
+var layerButtons = $('.layerButton');
+
+var i = layerButtons.length;
+while(i--) {
+  $(layerButtons[i]).html('Layer' + i).attr('id', 'layer' + i);
+};
+
+$('.layerButton').on('click', function() {
+  console.log($(this).attr('id'));
+})
