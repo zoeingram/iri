3138a1ec21403e71ba5c1d4811604082ef3dea46
Zoe Ingram <zoe.a.ingram@gmail.com>
2018-02-28 19:16:30 -0800

    italicizes footer

--- public/scripts/profile.js
+++ public/scripts/profile.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
   $('#newDocument').on('click', function() {
     console.log("yoo")
     $( function() {
-       $( "#createDocumentModal").dialog({show: 'fade', width: 400,height: 500});
+       $( "#createDocumentModal").dialog({show: 'fade', width: 400,height: 460});
      } );
   });
 })
