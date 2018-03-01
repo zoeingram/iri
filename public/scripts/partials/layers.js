689f60f7626862135d10058a56cafc494d9a9dca
Zoe Ingram <zoe.a.ingram@gmail.com>
2018-03-01 02:16:39 -0800

    toggles layer panel

--- public/scripts/partials/layers.js
+++ public/scripts/partials/layers.js
@@ -1,10 +1,14 @@
 var layerButtons = $('.layerButton');
+var layerInfo = $('.info');
 
+$(layerInfo).css('display', 'none');
 var i = layerButtons.length;
 while(i--) {
   $(layerButtons[i]).html('Layer' + i).attr('id', 'layer' + i);
+  $(layerInfo[i]).attr('id', 'info' + 'layer' +i);
 };
 
 $('.layerButton').on('click', function() {
-  console.log($(this).attr('id'));
+  var currentLayer = '#info' + $(this).attr('id');
+  $(currentLayer).toggle('fast');
 })
