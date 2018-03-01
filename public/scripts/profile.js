c514d5f8529fff4d64aaa4c3fbce1ccfbd8328db
Zoe Ingram <zoe.a.ingram@gmail.com>
2018-02-28 19:13:05 -0800

    styles create document modal

--- public/scripts/profile.js
+++ public/scripts/profile.js
@@ -1,7 +1,8 @@
 $(document).ready(function(){
   $('#newDocument').on('click', function() {
+    console.log("yoo")
     $( function() {
-       $( "#createDocumentModal" ).dialog({show: 'fade'});
+       $( "#createDocumentModal").dialog({show: 'fade', width: 400,height: 500});
      } );
   });
 })
