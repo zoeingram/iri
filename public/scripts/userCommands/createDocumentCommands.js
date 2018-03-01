98eca43ed058898a20065500c9fc819971bdeda5
Zoe Ingram <zoe.a.ingram@gmail.com>
2018-03-01 01:30:09 -0800

    adds create command

--- public/scripts/userCommands/createDocumentCommands.js
+++ public/scripts/userCommands/createDocumentCommands.js
@@ -1,32 +1,32 @@
-if(annyang) {
-  var createDocumentCommands = {
-      'document name *name': function(name){
-        $('#documentName').val(name);
-      },
+if (annyang) {
+    var createDocumentCommands = {
+        'document name *name': function(name) {
+            $('#documentName').val(name);
+        },
 
-      'next': function() {
-        // TODO: fix
-        $('.inputs').keypress(function() {
-          console.log("next");
-            $(this).next('.inputs').focus();
-        });
-      },
-      'length *length': function(length){
-        $('#documentWidth').val(length + 'px');
-      },
+        'next': function() {
+            // TODO: fix
+            $('.inputs').keypress(function() {
+                console.log("next");
+                $(this).next('.inputs').focus();
+            });
+        },
+        'length *length': function(length) {
+            $('#documentWidth').val(length + 'px');
+        },
 
-      'height *size': function(size){
-        console.log("hits height")
-        $('#documentHeight').val(size + 'px');
-      },
-      'create': function(){
-        console.log("creating");
-        // $('#createDocumentButton').click(); //TODO: make this the submit button when post is fixed
-        var createUrl = $('#createDocumentButton').attr('href');
-        window.location = createUrl;
-      }
-}
+        'height *size': function(size) {
+            console.log("hits height")
+            $('#documentHeight').val(size + 'px');
+        },
+        'create': function() {
+            console.log("creating");
+            // $('#createDocumentButton').click(); //TODO: make this the submit button when post is fixed
+            var createUrl = $('#createDocumentButton').attr('href');
+            window.location = createUrl;
+        }
+    }
 
-  annyang.addCommands(createDocumentCommands);
-  annyang.start();
+    annyang.addCommands(createDocumentCommands);
+    annyang.start();
 }
